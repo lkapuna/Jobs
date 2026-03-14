0e469486bb7e4c0a587a3bb34ef9fcf845f07dd4
lkapuna <lkapuna@gmail.com>
2026-03-14 12:54:44 +0200

    Add 'admin' role to senderRole enum in Message schema

--- models/Message.js
+++ models/Message.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+
 const messageSchema = new mongoose.Schema({
   chat: { type: String, required: true }, // jobId_workerId
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  senderRole: { type: String, enum: ['worker', 'employer'] },
+  senderRole: { type: String, enum: ['worker', 'employer', 'admin'] },
   text: { type: String, required: true },
   read: { type: Boolean, default: false },
 }, { timestamps: true });
 
+
 module.exports = mongoose.model('Message', messageSchema);
+
